81158be40ecc6d6e3edffd3c5f29cb7663d7e414
Sharad Shanbhag <sshanbhag@neomed.edu>
2017-01-12 19:14:00 -0500

    cleaning up merge mess

--- FlatWav.m
+++ FlatWav.m
@@ -2900,6 +2900,11 @@ function PeakTimeAdjCtrl_CreateFcn(hObject, eventdata, handles)
 			get(0,'defaultUicontrolBackgroundColor'))
 		 set(hObject,'BackgroundColor','white');
 	end
+function CalDisplayCtrl_CreateFcn(hObject, eventdata, handles)
+	if ispc && isequal(get(hObject,'BackgroundColor'), ...
+			get(0,'defaultUicontrolBackgroundColor'))
+		 set(hObject,'BackgroundColor','white');
+	end
 %******************************************************************************
 %******************************************************************************
 %******************************************************************************
